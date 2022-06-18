854af66ba184728f678c2c44aef2a3c639a52d91
ezequiel <ps3ezequiel@gmail.com>
2022-06-18 18:21:06 +0200

    Preparing planets page

---
+++ src/components/PlanetInfo.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const PlanetInfo = () => {
+  return (
+    <main>
+        <section>
+            <picture>
+                <img src=".#" alt=""/>
+            </picture>
+
+
+            <div>
+
+                <h1>
+
+                </h1>
+
+                <p>
+
+                </p>
+
+                <p>
+                    Source: 
+                    <a href=".#">wikipedia</a>
+                </p>
+            </div>
+        </section>
+        <section>
+
+            <div>
+                
+            </div>
+
+        </section>
+
+
+    </main>
+  )
+}
+
+
+export default PlanetInfo
